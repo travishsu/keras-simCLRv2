8c7eb98681c11ca265c5e4a10f259faa19c95040
howayi <fov223@gmail.com>
2020-06-25 10:53:41 +0800

    scratch skeleton & add runtime-cpu cmds

--- model.py
+++ model.py
@@ -1,1 +1,40 @@
 import tensorflow.keras as keras
+import tensorflow.keras.backend as K
+import tensorflow.keras.layers as KL
+import tensorflow.keras.models as KM
+import tensorflow.keras.utils as KU
+from tensorflow.keras.layers import Layer
+
+
+class DataGenerator(KU.Sequence):
+    def __init__(self):
+        pass
+
+    def __len__(self):
+        pass
+
+    def __getitem__(self, idx):
+        pass
+
+    def data_generator(self, image_ids):
+        pass
+
+    def on_epoch_end(self):
+        pass
+
+
+class SimCLRv2(object):
+    def __init__(self):
+        pass
+
+    def build(self):
+        pass
+
+    def compile(self):
+        pass
+
+    def save_h5(self):
+        pass
+
+    def train(self):
+        pass
